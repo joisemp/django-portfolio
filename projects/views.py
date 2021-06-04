b955f8850da4689e162130825a60884361c5d073
joisemp <joise1124@gmail.com>
2021-06-04 22:30:35 +0530

    removed unwanted download view

--- projects/views.py
+++ projects/views.py
@@ -10,12 +10,3 @@ class ProjectHomeView(ListView):
     template_name = 'projects/index.html'
     queryset = Project.objects.all()
     context_object_name = 'projects'
-
-def download(request, path):
-    file_path = os.path.join(settings.MEDIA_ROOT, path)
-    if os.path.exists(file_path):
-        with open(file_path, 'rb') as fh:
-            response=HttpResponse(fh.read(), content_type="application/image")
-            response['Content-Disposition']='inline;filename='+os.path.basename(file_path)
-            return response
-    raise Http404
