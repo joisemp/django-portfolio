eae692349b86afb12bf9cde060445a45edc56e11
joisemp <joise1124@gmail.com>
2021-06-04 22:24:18 +0530

    Created a userprofile model

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -27,6 +27,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+AUTH_USER_MODEL = 'projects.UserProfile'
 
 # Application definition
 
--- projects/models.py
+++ projects/models.py
@@ -1,6 +1,11 @@
 from django.db import models
 from django.db.models.signals import pre_delete
 from django.dispatch.dispatcher import receiver
+from django.contrib.auth.models import AbstractUser
+
+class UserProfile(AbstractUser):
+    pass
+
 
 class Project(models.Model):
     title = models.CharField(max_length=250)
