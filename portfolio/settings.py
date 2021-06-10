962f704910780f832e4de9f4489cf63cc6854621
joisemp <joise1124@gmail.com>
2021-06-10 09:37:26 +0530

    made some changes

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -139,7 +139,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # myaccount.google.com/apppasswords
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = '587'
-EMAIL_HOST_USER = 'contactjoise@gmail.com'
-EMAIL_HOST_PASSWORD = 'qwfwgpuurztdwchn'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
 EMAIL_USE_TLS = True
 # EMAIL_USE_SSL = False
